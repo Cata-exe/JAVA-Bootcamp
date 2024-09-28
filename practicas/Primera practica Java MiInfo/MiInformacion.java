512db0fba84ad13df997175473a49d5305bc4fbc
Cata-exe <catalina.hernandezf5@gmail.com>
2024-09-28 15:41:59 -0300

    Add actividad practica java

---
+++ practicas/Primera practica Java MiInfo/MiInformacion.java
@@ -0,0 +1,8 @@
+public class MiInformacion {
+    public static void main(String [] args){
+        System.out.println("Hola, mi nombre es Catalina Hernandez.");
+        System.out.println("Tengo 23 años de edad.");
+        System.out.println("Tengo una gatita llamada nina.");
+        System.out.println("Me gusta mucho programar y dibujar.");
+    }
+}
